f1d9df57037b854ed02da7d8e458a00e5c8e9d85
Pedro Bernardo <pedrovbernardo@gmail.com>
2023-08-15 01:17:26 -0300

    refactor: adjust filename and code format, variables names, early return, named regex, etc

---
+++ plugins/unicode-escaper.js
@@ -0,0 +1,18 @@
+const escapeUnicode = require('escape-unicode')
+
+const UNICODE_CHAR_REGEX = /[^\w\s\]+[^.,~+<>!@#$%(){}—&º_*-]/g
+const isNodeString = node => typeof node === 'string' && !/^\n\s*$/.test(node)
+const hasUnicodeChar = node => UNICODE_CHAR_REGEX.test(node)
+
+module.exports = function (options = {}, plugins = []) {
+  return function unicodeEscaper (tree) {
+    tree.walk(node => {
+      if (!isNodeString(node)) return node
+
+      return hasUnicodeChar(node)
+        ? node.replace(UNICODE_CHAR_REGEX, matched => escapeUnicode(matched))
+        : node
+    })
+    return tree
+  }
+}
